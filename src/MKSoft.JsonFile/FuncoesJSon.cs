ebd19b49cda1359c49ccc54c6721eb12b554ef0b
Marcelo Krol <marcelokrol@yahoo.com.br>
2021-10-07 14:21:45 -0300

    Inicial

---
+++ src/MKSoft.JsonFile/FuncoesJSon.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace MKSoft.JsonFile
+{
+   public static class FuncoesJSon
+   {
+      public static string ClasseParaJSon<T>(T obj)
+      {
+         try
+         {
+            return JsonConvert.SerializeObject(obj, Formatting.Indented);
+         }
+         catch(JsonException e)
+         {
+            throw new Exception(e.Message);
+         }
+      }
+      public static T JSonParaClass<T>(string jsonString)
+      {
+         try
+         {
+           return JsonConvert.DeserializeObject<T>(jsonString);
+         }
+         catch (JsonException e)
+         {
+            throw new Exception(e.Message);
+         }
+      }
+   }
+}
