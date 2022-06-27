9c7efaf859b2118876298ff9de750107cefe7685
Marcelo Krol <marcelokrol@yahoo.com.br>
2022-06-27 09:17:27 -0300

    Versao 2

--- src/MKSoft.JsonFile/FuncoesJSon.cs
+++ src/MKSoft.JsonFile/FuncoesJSon.cs
@@ -3,29 +3,29 @@ using Newtonsoft.Json;
 
 namespace MKSoft.JsonFile
 {
-   public static class FuncoesJSon
-   {
-      public static string ClasseParaJSon<T>(T obj)
-      {
-         try
-         {
-            return JsonConvert.SerializeObject(obj, Formatting.Indented);
-         }
-         catch(JsonException e)
-         {
-            throw new Exception(e.Message);
-         }
-      }
-      public static T JSonParaClass<T>(string jsonString)
-      {
-         try
-         {
-           return JsonConvert.DeserializeObject<T>(jsonString);
-         }
-         catch (JsonException e)
-         {
-            throw new Exception(e.Message);
-         }
-      }
-   }
+    public static class FuncoesJSon
+    {
+        public static string ClasseParaJSon<T>(T obj, bool indentar = true)
+        {
+            try
+            {
+                return JsonConvert.SerializeObject(obj, indentar ? Formatting.Indented : Formatting.None);
+            }
+            catch (JsonException e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+        public static T JSonParaClass<T>(string jsonString)
+        {
+            try
+            {
+                return JsonConvert.DeserializeObject<T>(jsonString);
+            }
+            catch (JsonException e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+    }
 }
